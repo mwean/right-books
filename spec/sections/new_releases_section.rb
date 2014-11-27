ef248273197ebe17aafe558ae52f13b28475a60c
Matt Wean <matthew.wean@gmail.com>
2014-11-26 20:46:34 -0600

    Fix new releases section

--- spec/sections/new_releases_section.rb
+++ spec/sections/new_releases_section.rb
@@ -1,6 +1,6 @@
 class NewReleasesSection < SitePrism::Section
   elements :books, '.book'
-  element :see_more, '.section__link a'
+  element :see_more, '.section__link'
 
   def click_book(book)
     books.find { |el| el.text.include?(book.title) }.click
