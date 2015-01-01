299253f904348254cf3a0b704c28a8c237da5bd8
Matt Wean <matthew.wean@gmail.com>
2015-01-01 15:27:20 -0600

    Add book editing to admin section

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,23 +1,15 @@
 module Admin
   class BooksController < AdminController
+    before_action :load_book, only: %i(edit update)
+
     def index
-      @books = Book.all
+      @books = Book.order(created_at: :desc)
     end
 
     def new
       amazon_result = AmazonResult.load(params[:isbn])
       @book = Book.from_amazon_result(amazon_result)
-
-      categories = ActiveModel::ArraySerializer.new(
-        Category.all,
-        each_serializer: CategorySerializer
-      )
-
-      gon.push(
-        categories: categories.as_json,
-        editor_notes: @book.editor_notes,
-        description: @book.description
-      )
+      update_gon
     end
 
     def create
@@ -25,6 +17,16 @@ module Admin
       book = Book.from_amazon_result(amazon_result)
       book.attributes = book_params
       book.save!
+
+      redirect_to admin_books_path
+    end
+
+    def edit
+      update_gon
+    end
+
+    def update
+      @book.update_attributes(book_params)
 
       redirect_to admin_books_path
     end
@@ -42,6 +44,30 @@ module Admin
     end
 
     private
+
+    def load_book
+      @book = Book.find_by(slug: params[:id])
+    end
+
+    def update_gon
+      gon.push(
+        categories: categories,
+        editor_notes: @book.editor_notes,
+        description: @book.description
+      )
+    end
+
+    def categories
+      Category.all.map do |category|
+        serialized_category = CategorySerializer.new(
+          category,
+          root: false,
+          selected_category_ids: @book.category_ids
+        )
+
+        serialized_category.as_json
+      end
+    end
 
     def book_params
       params.require(:book)
--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,3 +1,12 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :selected
+
+  def initialize(object, options = {})
+    @selected_category_ids = (options.delete(:selected_category_ids) || [])
+    super
+  end
+
+  def selected
+    @selected_category_ids.include?(object.id)
+  end
 end
