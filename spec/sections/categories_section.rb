ff80bd6205c2690005a11d50d2a655dd59915d38
Matt Wean <matthew.wean@gmail.com>
2015-02-08 17:17:40 -0800

    Guard against missing category link in specs

--- spec/sections/categories_section.rb
+++ spec/sections/categories_section.rb
@@ -2,6 +2,12 @@ class CategoriesSection < SitePrism::Section
   elements :category_links, 'a'
 
   def click_category(name)
-    category_links.find { |el| el.text.include?(name) }.click
+    wait_for_category_links
+
+    category_links.find { |el| el.text.include?(name) }.tap do |category_link|
+      raise "Browse link for category #{name} was not found" unless category_link
+
+      category_link.click
+    end
   end
 end
