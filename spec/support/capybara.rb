21adc6957d388ca32d17aaf4920e9eee7b6a385b
Matt Wean <matthew.wean@gmail.com>
2015-02-08 18:11:16 -0800

    Update poltergeist version and enable url blacklisting

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,7 +1,11 @@
 require 'capybara/poltergeist'
 
 Capybara.register_driver :poltergeist do |app|
-  Capybara::Poltergeist::Driver.new(app, js_errors: true, inspector: true)
+  Capybara::Poltergeist::Driver.new(
+    app,
+    js_errors: true,
+    phantomjs_options: ['--load-images=false']
+  )
 end
 
 Capybara.register_driver :chrome do |app|
@@ -9,4 +13,13 @@ Capybara.register_driver :chrome do |app|
 end
 
 Capybara.default_driver = :rack_test
-Capybara.javascript_driver = ENV['CHROME'] ? :chrome : :poltergeist
+Capybara.javascript_driver = (ENV['CHROME'] || ENV['CI']) ? :chrome : :poltergeist
+
+RSpec.configure do |config|
+  config.before(:each, js: true) do
+    page.driver.browser.url_blacklist = [
+      'http://use.typekit.net/zpc4pgn.js',
+      'http://example.com/image.jpg'
+    ]
+  end
+end
